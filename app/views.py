302a5335b4b263789f642ffc869195302077d268
majinlion <amitkgpt@gmail.com>
2016-06-12 20:20:53 +0530

    added response handling

--- app/views.py
+++ app/views.py
@@ -2,10 +2,34 @@ from app import app
 from pymongo import MongoClient
 import json
 from bson import ObjectId
-
+import requests
+from jsonencoder import JSONEncoder
+from bson import BSON
+from bson import json_util,ObjectId
+from response import *
 client = MongoClient('mongodb://localhost:27017/')
 db = client.tms
 
+
+
+
+'''
+Every software firm uses some sort of ticketing system to easily track the tasks that are being performed.
+A user must be able to create and update a ticket (with title, description, category, Priority, Status, Resolution, Start date/End date , Reporter etc). Also the ticket should be assignable to a user. User must have the ability to comment on the ticket. There must be feature to get all the tickets for a user and get comments for the tickets as well. In addition, there must be ability to filter them based on assigned user, status, resolution or start/end date. 
+
+Hints-
+  - Create new tickets
+  - Assign a ticket to a user
+  - Change status or resolution of a ticket
+  - Get all tickets associated with user (with filtering and pagination options)
+  - Fetch comments associated with the ticket (with pagination option)
+
+Bonus points:
+- Should support the feature to track and show the activity history
+- Add Tests or perform TDD
+
+You are free to use any technologies you want. Create database structure to support the feature. 
+'''
 
 @app.route('/')
 def home_page():
@@ -30,7 +54,12 @@ def assign_tickets():
 
 @app.route('/get_tickets')
 def get_tickets():
-	return "find"
+	try:
+		http_response = CustomResponse(200,"Get tickets successful" , manager.get_tickets(request.data))
+		return http_response.get_success_message()
+	except:
+		http_response = CustomResponse(11111,"There was some exception in getting tickets" , "")
+		return http_response.get_failed_message()
 
 @app.route('/get_users')
 def get_users():
